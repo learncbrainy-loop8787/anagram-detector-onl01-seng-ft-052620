495042e061bf481e03849dbee659a0b50279c787
korey ko <koreykendrick1@gmail.com>
2020-06-06 03:56:35 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+
+class Anagram 
+  
+  attr_accessor :name
+   
+   def initialize(name)
+     @name = name
+end
+
+def match(array_of_words)
+   array_of_words.select do |array|
+      (@name.split("").sort) == (array.split("").sort)
+    end
+end
+end
